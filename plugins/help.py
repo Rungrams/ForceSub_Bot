a3324feaccd329306f2f86fb439bbe5f441a0329
RUNGRAM <87849536+Rungrams@users.noreply.github.com>
2021-08-05 14:45:53 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -127,7 +127,7 @@ def _help(client, message):
         except Exception:
             client.send_message(
                 chat_id=message.chat.id,
-                text="Hey use this command in my pm. \nFor more help ask in my [Support Group](https://t.me/UniversalBotsSupport).",
+                text="Hey use this command in my pm. \nFor more help ask in my [Support Group](https://t.me/tobotupdate).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
